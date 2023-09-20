7bc09d8cedb3f6b38a3efb1ca25334ac62ceef74
Patrick B Warren <patrickbwarren@gmail.com>
2023-09-20 12:57:07 +0100

    small correction by Nat Dalton

--- extra/old3d2json.py
+++ extra/old3d2json.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-# Copyright (c) 2019 Patrick B Warren
+# Original code copyright (c) 2019 Patrick B Warren
+# Modifications copyright (c) 2023 Nat Dalton
 
 # Distributed under the terms of the GNU General Public License v2
 
@@ -10,6 +11,9 @@
 # For example ./old3d2json.py --epsg=31255 1627_65.3d > 1627_65.json
 
 # EPSG:31255 is MGI / Austria GK Central CRS
+
+# Small correction (2023) by Nat Dalton at line 41 in the current file
+# adds a check for an empty list of fields.
 
 import sys
 import json
@@ -35,19 +39,20 @@ names, coords = [], []
 with open(filename) as f:
     for record in f:
         fields = record.split()
-        if fields[0] in ['move', 'draw', 'name']:
-            xyz = [float(v) for v in fields[-3:]] # last three fields
-            if fields[0] == 'move':
-                if line: # catch the last line string
-                    lines.append(line)
-                line = [xyz] # start a new line string
-            elif fields[0] == 'draw':
-                line.append(xyz) # grow the current line string
-            elif fields[0] == 'name':
-                if line: # catch the last line string
-                    lines.append(line)
-                names.append(fields[1]) # keep the name..
-                coords.append(xyz) # .. and the position
+        if fields: # added empty list check for fields (ND mod)
+            if fields[0] in ['move', 'draw', 'name']:
+                xyz = [float(v) for v in fields[-3:]] # last three fields
+                if fields[0] == 'move':
+                    if line: # catch the last line string
+                        lines.append(line)
+                    line = [xyz] # start a new line string
+                elif fields[0] == 'draw':
+                    line.append(xyz) # grow the current line string
+                elif fields[0] == 'name':
+                    if line: # catch the last line string
+                        lines.append(line)
+                    names.append(fields[1]) # keep the name..
+                    coords.append(xyz) # .. and the position
 
 # create a GIS data structure as a hierarchy of dicts and lists
 
